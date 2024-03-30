bd812ce4c7b9481d0f4460d95afbbd182ae1e929
SuicideG4M3R <suicideg4m3r@gmail.com>
2024-03-30 23:27:22 +0100

    User View, Settings

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Institution
 
-# Register your models here.
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'get_categories_list')
+    search_fields = ('name', 'type')
+    list_filter = ('type',)
+
+    def get_categories_list(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+
+    get_categories_list.short_description = 'Categories'
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-
-from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib.auth.models import User
 from django.db.utils import ProgrammingError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-
 from .forms import RegisterForm, LoginForm
 from .models import Donation, Institution, Category
 from django.db.models import Sum
@@ -66,8 +65,6 @@ class AddDonationView(View):
             pick_up_date = datetime.strptime(pick_up_date_str, '%Y-%m-%d').date()
         except ValueError:
             return HttpResponse(f'Error daty: {pick_up_date_str}')
-        print(quantity, categories_ids, institution_id, address, city, zip_code, pick_up_date, pick_up_time,
-              pick_up_comment)
         donation = Donation.objects.create(
             quantity=quantity,
             institution_id=institution_id,
@@ -154,3 +151,76 @@ class LogoutView(View):
 class ConfirmationView(View):
     def get(self, request):
         return render(request, 'form-confirmation.html')
+
+
+class UserView(View):
+    def get(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return redirect('Login')
+        context = {
+            'donations': Donation.objects.filter(user=user),
+        }
+        return render(request, 'user.html', context)
+
+
+class MarkTakenView(View):
+    def post(self, request, id):
+        if Donation.objects.filter(id=id).exists():
+            donation = Donation.objects.get(id=id)
+            if donation.is_taken:
+                donation.is_taken = False
+            else:
+                donation.is_taken = True
+            donation.save()
+            return redirect('user')
+        else:
+            return redirect('home')
+
+
+class SettingsView(View):
+    def get(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return redirect('Login')
+        return render(request, 'settings.html')
+
+    def post(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return redirect('Login')
+
+        old_password = request.POST.get('old_password')
+        new_password1 = request.POST.get('new_password1')
+        new_password2 = request.POST.get('new_password2')
+
+        if new_password1 != '':
+            if new_password1 == new_password2:
+                if user.check_password(old_password):
+                    user.set_password(new_password1)
+                    user.save()
+                    update_session_auth_hash(request, user)
+                    messages.success(request, "Hasło zostało pomyślnie zmienione.")
+                else:
+                    messages.error(request, "Aktualne hasło jest niepoprawne.")
+                    return redirect('Settings')
+            else:
+                messages.error(request, "Powtórzone hasło jest niepoprawne.")
+                return redirect('Settings')
+
+        username = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        if username != user.username or first_name != user.first_name or last_name != user.last_name:
+            if username != user.username and User.objects.filter(username=username).exists():
+                messages.error(request, "Podany email już istnieje.")
+            else:
+                if user.check_password(old_password):
+                    user.username = username
+                    user.first_name = first_name
+                    user.last_name = last_name
+                    user.save()
+                    messages.success(request, "Dane osobowe zostały pomyślnie zaktualizowane.")
+                else:
+                    messages.error(request, "Podaj aktualne hasło aby zmienić dane")
+        return redirect('Settings')
